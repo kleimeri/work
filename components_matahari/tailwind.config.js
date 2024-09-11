3dc96db92a7709beb8ed26cd12997b0c689c64a8
kleimeri <klimopjesys@gmail.com>
2024-09-11 18:35:15 +0300

    awd

---
+++ components_matahari/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// npx tailwindcss -i ./assets/css/input.css -o ./assets/css/output.css --watch
+module.exports = {
+  content: ['./componets/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
